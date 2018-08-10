44d89f3d374098f99e7553869338a7aeed220182
alden alden <alden.hari@gmail.com>
2018-08-10 06:30:43 +0000

    Update index.js adding some brake on processes.

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,18 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'analytics';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 router.post('/report', function(req, res) {
   let data = req.body
   
   MongoClient.connect(url, {useNewUrlParser:true}, function(err, client) {
-    if (err) res.status(400).json(err);
+    if (err) return res.status(400).json(err);
     const db = client.db(dbName);
     const collectionName = 'email_reporting';
     const collection = db.collection(collectionName);
     // Insert one documents
     collection.insertOne(data, function(err, r) {
-      if (err) res.status(400).json(err);
-      res.send(r);
+      if (err) return res.status(400).json(err);
+      return res.send(r);
     });
   });
   
